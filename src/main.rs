ee981ac323b8935b5ce559d3107b62ff56e30585
Sparrow0hawk <alexjcoleman@gmail.com>
2022-06-02 16:12:50 +0100

    a depth 2 tree generator, output is rubbish

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 #![allow(unused)]
 use clap::Parser;
+use std::fs;
 
-#[derive(Parser)]
+#[derive(Parser, Debug)]
 struct Cli {
     // path to create tree from
     #[clap(parse(from_os_str))]
@@ -10,4 +11,18 @@ struct Cli {
 
 fn main() {
     let args = Cli::parse();
+
+    let paths = fs::read_dir(&args.path).unwrap();
+
+    for path in paths {
+        let path_result = path.unwrap();
+
+        println!("{}", path_result.path().display());
+
+        if fs::metadata(path_result.path()).unwrap().is_dir() {
+            for sub_path in fs::read_dir(path_result.path()).unwrap() {
+                println!(" -- {}", sub_path.unwrap().path().display())
+            }
+        }
+    }
 }
